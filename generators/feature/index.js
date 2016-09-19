6ba069fc6b05b8b2dd5cdbdd44834e0af5eda07c
Melvin Rodriguez <melvin.rodriguez@prodigious.cr>
2016-09-19 15:56:38 -0600

    init

---
+++ generators/feature/index.js
@@ -0,0 +1,86 @@
+'use strict';
+var generators = require('yeoman-generator');
+var yosay = require('yosay');
+var chalk = require('chalk');
+var path = require('path');
+var guid = require('node-uuid');
+
+module.exports = generators.Base.extend({
+
+    prompting: function() {
+        this.log(yosay(
+            'Welcome to the slick ' + chalk.red.bold('Prodigious Helix') + ' generator!'
+        ));
+
+        console.log('INFO: .NET Framework 4.5');
+        console.log('INFO: MVC 5.2.3');
+        console.log('');
+        console.log(chalk.red.bold('YOU MUST RUN THIS GENERATOR AS AN ADMINISTRATOR.'));
+        console.log('');
+
+        return this.prompt([{
+            type: 'input',
+            name: 'solutionName',
+            message: 'Enter the name of your Solution:',
+            default: 'Sitecore'
+        }, {
+            type: 'input',
+            name: 'featureTitle',
+            message: 'Enter the name of your Feature module:'
+        }]).then(function(answers) {
+            this.props = answers;
+            this.props.projectGuid = '{' + guid.v4() + '}';
+        }.bind(this));
+    },
+    writing: function() {
+        var targetPath = path.join('src', 'Feature', this.props.featureTitle);
+        console.log('Target Path: ' + targetPath);
+
+        /*********** CODE ***************/
+        this.fs.copy(
+            this.templatePath('code/**/*'),
+            this.destinationPath(path.join(targetPath, 'code'))
+        );
+
+        // csproj
+        this.fs.copyTpl(
+            this.templatePath('Sitecore.Feature.csproj'),
+            this.destinationPath(path.join(targetPath, 'code', this.props.solutionName + '.Feature.' + this.props.featureTitle + '.csproj')),
+            this.props
+        );
+
+        // // AssemblyInfo.cs, project
+        this.fs.copyTpl(
+            this.templatePath('AssemblyInfo.cs'),
+            this.destinationPath(path.join(targetPath, 'code', 'Properties', 'AssemblyInfo.cs')), { assemblyName: this.props.solutionName + '.Feature.' + this.props.featureTitle }
+        );
+
+        // // Publish Profile configuration
+        this.fs.copyTpl(
+            this.templatePath('Local.pubxml'),
+            this.destinationPath(path.join(targetPath, 'code', 'Properties/PublishProfiles', 'Local.pubxml')), { assemblyName: this.props.solutionName + '.Foundation.' + this.props.featureTitle }
+        );
+
+        // // config
+        this.fs.copyTpl(
+            this.templatePath('Feature.config'),
+            this.destinationPath(path.join(targetPath, 'code', 'App_Config', 'Include', 'Feature', 'Feature.' + this.props.featureTitle + '.config')),
+            this.props
+        );
+
+        // tds
+        this.fs.copy(
+            this.templatePath('tds/**/*'),
+            this.destinationPath(path.join(targetPath, 'tds'))
+        );
+    },
+    end: function() {
+        console.log('');
+        console.log('Solution name ' + chalk.red.bold(this.props.solutionName));
+        console.log('Your Feature module ' + chalk.red.bold(this.props.featureTitle) + ' has been created');
+        console.log('');
+        console.log('You will need to add your Feature project(s) to your Visual Studio solution.');
+        console.log('Then build and publish the Feature project from Visual Studio.');
+        console.log('');
+    }
+});
---
+++ generators/foundation/index.js
@@ -0,0 +1,80 @@
+'use strict';
+var generators = require('yeoman-generator');
+var yosay = require('yosay');
+var chalk = require('chalk');
+var path = require('path');
+var guid = require('node-uuid');
+
+module.exports = generators.Base.extend({
+
+    prompting: function() {
+        this.log(yosay(
+            'Welcome to the slick ' + chalk.red.bold('Prodigious Helix') + ' generator!'
+        ));
+
+        console.log('INFO: .NET Framework 4.5');
+        console.log('INFO: MVC 5.2.3');
+        console.log('');
+        console.log(chalk.red.bold('YOU MUST RUN THIS GENERATOR AS AN ADMINISTRATOR.'));
+        console.log('');
+
+        return this.prompt([{
+            type: 'input',
+            name: 'solutionName',
+            message: 'Enter the name of your Solution:',
+            default: 'Sitecore'
+        }, {
+            type: 'input',
+            name: 'foundationTitle',
+            message: 'Enter the name of your Foundation module:'
+        }]).then(function(answers) {
+            this.props = answers;
+            this.props.projectGuid = '{' + guid.v4() + '}';
+        }.bind(this));
+    },
+    writing: function() {
+        var targetPath = path.join('src', 'Foundation', this.props.foundationTitle);
+        console.log('Target Path: ' + targetPath);
+
+        /*********** CODE ***************/
+        this.fs.copy(
+            this.templatePath('code/**/*'),
+            this.destinationPath(path.join(targetPath, 'code'))
+        );
+
+        // csproj
+        this.fs.copyTpl(
+            this.templatePath('Sitecore.Foundation.csproj'),
+            this.destinationPath(path.join(targetPath, 'code', this.props.solutionName + '.Foundation.' + this.props.foundationTitle + '.csproj')),
+            this.props
+        );
+
+        // // AssemblyInfo.cs, project
+        this.fs.copyTpl(
+            this.templatePath('AssemblyInfo.cs'),
+            this.destinationPath(path.join(targetPath, 'code', 'Properties', 'AssemblyInfo.cs')), { assemblyName: this.props.solutionName + '.Foundation.' + this.props.foundationTitle }
+        );
+
+        // // Publish Profile configuration
+        this.fs.copyTpl(
+            this.templatePath('Local.pubxml'),
+            this.destinationPath(path.join(targetPath, 'code', 'Properties/PublishProfiles', 'Local.pubxml')), { assemblyName: this.props.solutionName + '.Foundation.' + this.props.foundationTitle }
+        );
+
+        // // config
+        this.fs.copyTpl(
+            this.templatePath('Foundation.config'),
+            this.destinationPath(path.join(targetPath, 'code', 'App_Config', 'Include', 'Foundation', 'Foundation.' + this.props.foundationTitle + '.config')),
+            this.props
+        );
+    },
+    end: function() {
+        console.log('');
+        console.log('Solution name ' + chalk.red.bold(this.props.solutionName));
+        console.log('Your foundation module ' + chalk.red.bold(this.props.foundationTitle) + ' has been created');
+        console.log('');
+        console.log('You will need to add your foundation project(s) to your Visual Studio solution.');
+        console.log('Then build and publish the foundation project from Visual Studio.');
+        console.log('');
+    }
+});
